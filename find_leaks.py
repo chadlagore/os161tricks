8e79b5f3a1fdd1c869ba41630304f2bf567d7ea3
chadlagore <chad.d.lagore@gmail.com>
2017-03-05 17:08:09 -0800

    comments

--- find_leaks.py
+++ find_leaks.py
@@ -6,10 +6,16 @@ filename = 'output.csv'
 
 
 def get_leak_table():
+    '''
+    Return the leak table (in filename)
+    '''
     return pd.read_csv(filename, names=['size', 'address'])
 
 
 def clean_tbl(tbl):
+    '''
+    Return a cleaned up table. Remove excess strings etc.
+    '''
     tbl['address'] = (tbl['address']
                       .replace(regex=True, to_replace='allocated at', value=''))
     tbl['size'] = (tbl['size']
@@ -19,19 +25,23 @@ def clean_tbl(tbl):
     return tbl
 
 
-def print_results(tbl):
+def call_addr2line(tbl):
+    '''
+    Call os161 addr2line function.
+    '''
     for ix in tbl.index:
         addr = tbl.loc[ix]['address']
         syscall = 'os161-addr2line -e ~/os161/root/kernel {}'.format(addr)
         response = os.popen(syscall).read()[40:]
         tbl['line'].loc[ix] = response
-    print(tbl)
+    return tbl
 
 
 def main():
     tbl = get_leak_table().drop_duplicates(subset='address')
     tbl = clean_tbl(tbl).reset_index()[['size', 'address', 'line']]
-    print_results(tbl)
+    tbl = call_addr2line(tbl)
+    print(tbl)
 
 
 if __name__ == '__main__':
